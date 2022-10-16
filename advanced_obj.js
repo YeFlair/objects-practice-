f866a08458088fd45c10f9abe03f70cb3b0a5a25
Section9-Togusa <corbindac@gmail.com>
2022-10-16 15:00:46 -0700

    final practice

--- advanced_obj.js
+++ advanced_obj.js
@@ -54,4 +54,55 @@ const robot3 = {
   console.log(robot3.numOfSensors)
   
   
-  
+console.log("_______________________")
+console.log("Advanced Objects - Factory Function\n")
+
+const robotFactory = (model, mobile) => {
+    return {
+        model : model, 
+        mobile, // destructuring (property value shorthand) allows the property to stand alone - unlike how model is repeated. 
+        beep () {
+            console.log('Bleep Bloop')
+        }
+    }
+}
+
+const terminator = robotFactory("Good Guy", "yes")
+console.log(terminator.mobile)
+
+
+console.log("_______________________")
+console.log("Advanced Objects - Deconstruction Assignment\n")
+
+const newRobot = {
+    model : '1E78V2', 
+    mobile : true, 
+    functionality : {
+        beep () {
+            console.log('Beep Beep')
+        }, 
+        fireLaser () {
+            console.log("Pew Pew")
+        }
+    }
+}
+
+const {functionality} = newRobot // deconstructed assignment is an object key wrapped in curly braces 
+functionality.beep()
+
+console.log("_______________________")
+console.log("Advanced Objects - Built-in Object Methods\n")
+
+const newRobot2 = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+const newRobot2Keys = Object.keys(newRobot2) // returns an array of the keys inside 
+console.log(newRobot2Keys)
+
+const newRobot2Entries = Object.entries(newRobot2) // returns an array of both the key and value of the properties in an object.
+console.log(newRobot2Entries)
